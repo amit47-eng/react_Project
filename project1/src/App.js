fa5f17902907b8b4d0dbd4caf90c5335a6a7cf8e
amit47-eng <av495267@gmail.com>
2024-11-28 21:18:34 +0530

    adding the Body.js and the css par

---
+++ project1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import Body from './component/Web_Body/body'
+
+function App(){
+    return(
+        
+        <div>
+            <p className="head_intro">
+           <h1>Hii this is the first project i made myselfnp</h1>
+           </p>
+           <Body/>
+        </div>
+        
+    )
+}
+export default App
---
+++ project1/src/component/Web_Body/body.js
@@ -0,0 +1,12 @@
+import "./Body.css"
+
+function Body(){
+    return(
+        <div className="body_container"> 
+            <h2 className="body_head">Main Data</h2>
+
+        </div>
+    )
+}
+
+export default Body
